37b12d8f9f00cf2285d22649506786e5ce23e6bb
Frank <archerfrank@gmail.com>
2020-07-26 19:09:12 +0800

    Minikube with virtual box

--- Chapter15/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
+++ Chapter15/microservices/product-composite-service/src/main/java/se/magnus/microservices/composite/product/ProductCompositeServiceApplication.java
@@ -8,18 +8,11 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.reactive.function.client.WebClient;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 import static java.util.Collections.emptyList;
 import static org.springframework.web.bind.annotation.RequestMethod.*;
-import static springfox.documentation.builders.RequestHandlerSelectors.basePackage;
-import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
 
-@EnableSwagger2WebFlux
+
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductCompositeServiceApplication {
@@ -34,33 +27,6 @@ public class ProductCompositeServiceApplication {
     @Value("${api.common.contact.url}")       String apiContactUrl;
     @Value("${api.common.contact.email}")     String apiContactEmail;
 
-	/**
-	 * Will exposed on $HOST:$PORT/swagger-ui.html
-	 *
-	 * @return
-	 */
-	@Bean
-	public Docket apiDocumentation() {
-
-		return new Docket(SWAGGER_2)
-			.select()
-			.apis(basePackage("se.magnus.microservices.composite.product"))
-			.paths(PathSelectors.any())
-			.build()
-				.globalResponseMessage(POST, emptyList())
-				.globalResponseMessage(GET, emptyList())
-				.globalResponseMessage(DELETE, emptyList())
-				.apiInfo(new ApiInfo(
-                    apiTitle,
-                    apiDescription,
-                    apiVersion,
-                    apiTermsOfServiceUrl,
-                    new Contact(apiContactName, apiContactUrl, apiContactEmail),
-                    apiLicense,
-                    apiLicenseUrl,
-                    emptyList()
-                ));
-    }
 
 	@Bean
 	@LoadBalanced
